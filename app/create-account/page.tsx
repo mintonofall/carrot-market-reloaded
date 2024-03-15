9b8268797e674d28b9d92a404e6293afee29e9ba
mintonofall <mintonofall@gmail.com>
2024-03-15 23:18:34 +0900

    UI 개선 및 버그 수정***
    ***번역: UI improvements and bug fixes

--- app/create-account/page.tsx
+++ app/create-account/page.tsx
@@ -1,23 +1,34 @@
 import Link from "next/link";
+import { ChatBubbleOvalLeftIcon } from "@heroicons/react/24/outline";
 
 export default function CreateAccount() {
   return (
-    <div>
-      <div>
+    <div className="flex flex-col gap-10 px-8 py-6">
+      <div className="flex flex-col gap-2 *:font-medium">
         <h1>안녕하세요</h1>
         <h2>Fill in the form below to join</h2>
       </div>
-      <form>
-        <div>
-          <input type="text" placeholder="Username" required />
-          <span>Input error</span>
+      <form className="flex flex-col gap-3">
+        <div className="flex flex-col gap-2">
+          <input
+            className="bg-transparent rounded-md w-full h-10 focus:outline-none focus:ring-2 ring-neutal-200 placeholder:text-neutral-400 focus:ring-orange-500"
+            type="text"
+            placeholder="Username"
+            required
+          />
+          <span className="text-red-500">Input error</span>
         </div>
-        <button>Create account</button>
+        <button className="primary-btn py-2.5">Create account</button>
       </form>
-      <div />
+      <div className="w-full h-px bg-neutral-500" />
       <div>
-        <Link href="/sms">
-          <span>icon</span>
+        <Link
+          className="primary-btn flex h-10 items-center justify-cente gap-2 w-full px-3 py-2.5 rounded-md bg-orange-500 text-white font-medium hover:bg-orange-600 focus:outline-none focus:ring-2 ring-orange-500 items-center justify-center gap-2 w-full px-3 py-2.5 rounded-md bg-orange-500 text-white font-medium hover:bg-orange-600 focus:outline-none focus:ring-2 ring-orange-500"
+          href="/sms"
+        >
+          <span>
+            <ChatBubbleOvalLeftIcon className="text-white h-6" />
+          </span>
           <span>Sign up with SMS</span>
         </Link>
       </div>
