7bae1309c233835a07b5b89748a9c09047f2c118
mintonofall <mintonofall@gmail.com>
2024-03-16 10:15:48 +0900

    로그인 페이지 수정 및 API 연동

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,19 @@
+"use client";
 import Link from "next/link";
 import { ChatBubbleOvalLeftIcon } from "@heroicons/react/24/outline";
 import FormInput from "@/components/FormInput";
 import FormButton from "@/components/form-btn";
 import SocialLogin from "@/components/social-login";
 
-export default function CreateAccount() {
+export default function Login() {
+  const onClick = async () => {
+    const response = await fetch("/www/users", {
+      method: "POST",
+      body: JSON.stringify({ username: "nico", password: "1234" }),
+    });
+    console.log(await response.json());
+  };
+
   return (
     <div className="flex flex-col gap-10 px-8 py-6">
       <div className="flex flex-col gap-2 *:font-medium">
@@ -21,8 +30,10 @@ export default function CreateAccount() {
             errors={[]}
           />
         </div>
-        <FormButton loading={false} text="Create Account" />
       </form>
+      <span onClick={onClick}>
+        <FormButton loading={false} text="Log In" />
+      </span>
       <div className="w-full h-px bg-neutral-500" />
       <div className="flex flex-col gap-2">
         <SocialLogin />
