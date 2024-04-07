dbe28c845327d973c07358a9cb0949cad943a62a
mintonofall <mintonofall@gmail.com>
2024-04-07 22:01:24 +0900

    코드 변경 내용을 요약한 간결한 커밋 메시지입니다.

---
+++ app/middleware.ts
@@ -0,0 +1,11 @@
+import { NextRequest } from "next/server";
+import getSession from "./lib/session";
+
+export async function middleware(requrst: NextRequest) {
+ const session = await getSession();
+ 
+}
+
+export const config = {
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
