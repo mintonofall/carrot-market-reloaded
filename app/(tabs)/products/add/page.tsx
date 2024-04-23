af9ca75f639df567a25c550588a268a7b06a2efb
mintonofall <mintonofall@gmail.com>
2024-04-23 20:18:50 +0900

    제품 추가 페이지 업데이트

--- app/(tabs)/products/add/page.tsx
+++ app/(tabs)/products/add/page.tsx
@@ -1,3 +1,38 @@
+"use client";
+import { PhotoIcon } from "@heroicons/react/24/outline";
+import FormInput from "@/components/FormInput";
+import FormButton from "@/components/form-btn";
 export default function AddProduct() {
-  return <h1>Add Product</h1>;
+  const onImageChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log("event", event);
+    const {
+      target: { files },
+    } = event;
+  };
+  console.log();
+
+  return (
+    <div>
+      <form className="flex flex-col gap-3 p-5">
+        <label
+          htmlFor="photo"
+          className="border-2 aspect-square flex justify-center items-center flex-col border-dashed border-neutral-200 rounded-lg cursor-pointer"
+        >
+          <PhotoIcon className="size-20"></PhotoIcon>
+          <div>사진을 추가하세요</div>
+        </label>
+        <input
+          onChange={onImageChange}
+          type="file"
+          id="photo"
+          name="photo"
+          className="hidden"
+        />
+        <FormInput name="title" placeholder="제목" type="text" />
+        <FormInput name="price" type="number" placeholder="가격" />
+        <FormInput name="description" type="text" placeholder="자세한 설명" />
+        <FormButton text="작성 완료" />
+      </form>
+    </div>
+  );
 }
