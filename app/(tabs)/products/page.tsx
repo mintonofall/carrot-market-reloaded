31696bbe72bb6c40b453daeb22638e5cb1953ae1
mintonofall <mintonofall@gmail.com>
2024-04-23 13:50:44 +0900

    상품 페이지에 상품 추가 버튼 추가

--- app/(tabs)/products/page.tsx
+++ app/(tabs)/products/page.tsx
@@ -1,6 +1,8 @@
 import db from "@/lib/db";
 import Image from "next/image";
 import ListProduct from "@/components/list-product";
+import Link from "next/link";
+import { PlusIcon } from "@heroicons/react/24/outline";
 
 async function getProducts() {
   const products = await db.product.findMany({
@@ -19,11 +21,20 @@ export default async function Product() {
   const products = await getProducts();
   return (
     <div>
-      {products.map((product) => (
-        <div className="py-2">
-          <ListProduct key={product.id} {...product} />
-        </div>
-      ))}
+      <div>
+        {products.map((product) => (
+          <div className="py-2">
+            <ListProduct key={product.id} {...product} />
+          </div>
+        ))}
+      </div>
+      <div>
+        <Link href="/products/add">
+          <div className="bg-orange-500 flex items-center justify-center rounded-full fixed bottom-20 right-8 size-20 p-5 ">
+            <PlusIcon className="text-white"></PlusIcon>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 }
